69580c15c63af645700af2bb80a5fbe4ebb4ddd2
Dharshan A <102800724+Tringapps-Dharshan@users.noreply.github.com>
2022-06-29 21:18:23 +0530

    update

--- src/sharedpages/PreviewBackdrop.tsx
+++ src/sharedpages/PreviewBackdrop.tsx
@@ -1,16 +1,42 @@
+import { useState } from 'react'
 import './PreviewBackdrop.scss'
 import { TextField, Select, Button, Backdrop, Box, Avatar } from '@mui/material'
+import { useForm, useFieldArray, Controller, useWatch } from 'react-hook-form';
+
 type handleClose = () => void
+
 type setOpen = (set: boolean) => void
+
 export type props = {
     user: number,
     open: boolean,
     setOpen: setOpen
 }
+
 function PreviewBackdrop({ open, setOpen, user }: props) {
+
+    const [product, setProduct] = useState('');
+    const [quantity, setQuantity] = useState(1);
+
     const handleClose: handleClose = () => {
         setOpen(false);
     };
+
+    const handleStock = () => {
+        console.log(user);
+    }
+
+    const { register, control, handleSubmit, watch } = useForm({
+        defaultValues: {
+            data: [{ product: "", quantity: 1 }]
+        }
+    });
+
+    const { fields, append, remove } = useFieldArray({
+        control,
+        name: "data"
+    });
+
     return (
         <Backdrop
             open={open}
@@ -18,58 +44,89 @@ function PreviewBackdrop({ open, setOpen, user }: props) {
             <Box sx={{ color: 'black', backgroundColor: 'white', borderRadius: '5px', boxShadow: 'rgba(0, 0, 0, 0.56) 0px 22px 70px 4px' }}>
                 <div className='box-header'>
                     <Avatar sx={{ margin: '10px', bgcolor: 'tomato' }}>
-                        
+
                     </Avatar>
                     <p className="cus-name"></p>
                 </div>
-                <div className='box-content'>
-                    <div className='content'>
-                        <Select
-                            sx={{ minWidth: 120 }}
-                            size="small"
-                            
-                        >
-                            
-                        </Select>
+                <form onSubmit={handleSubmit(() => handleStock())}>
+                    <div>
+                        {fields.map((item, index) => {
+                            return (
+                                <div key={item.id} className='box-content'>
+                                    <div className='content'>
+                                        <p className='box-title'>Select Product</p>
+                                        <select
+                                            {...register(`data.${index}.product`)}
+                                        >
+                                            <option disabled value="">SELECT</option>
+                                        </select>
+                                    </div>
+                                    <div className='content'>
+                                        <p className='box-title'>Price</p>
+                                    </div>
+                                    <div className='content'>
+                                        <p className='box-title'>Custom Quantity</p>
+                                        <Controller
+                                            render={({ field }) =>
+                                                <input {...field}
+                                                    value={quantity}
+                                                    type="number"
+                                                    min="1"
+                                                // max={quanInStock}
+                                                // onChange={e => setQuantity(parseInt(e.target.value))}
+                                                // disabled={product.length == 0 ? true : false}
+                                                />}
+                                            name={`data.${index}.quantity`}
+                                            control={control}
+                                        />
+                                    </div>
+                                    <div className='content'>
+                                        <p className='box-title'>Total Amount</p>
+                                        {/* <p>{product.length === 0 && "-"}</p>
+                                        <p>{quantity === 0 ? "-" : product && selectedProduct && quantity * selectedProduct.product_price}</p> */}
+                                    </div>
+                                    <div className='content'>
+                                        <button type="button" onClick={() => remove(index)}>
+                                            Delete
+                                        </button>
+                                    </div>
+                                    <div className='content'>
+                                        <button
+                                            type='button'
+                                            onClick={() => {
+                                                append({ product: product, quantity: quantity });
+                                            }}
+                                        >
+                                            Add
+                                        </button>
+                                    </div>
+                                </div>
+                            );
+                        })}
                     </div>
-                    <div className='content'>
-                        
+                    {/* {error && product.length === 0 && <Alert severity="error">Select Product</Alert>} */}
+
+                    <div className="cus-action">
+                        <div>
+                            <Button
+                                type='submit'
+                                variant="contained"
+                                disabled={quantity === 0 ? true : false}
+                            >
+                                Supply
+                            </Button>
+                        </div>
+                        <div>
+                            <Button
+                                variant="contained"
+                                sx={{ margin: '10px' }}
+                                onClick={handleClose}
+                            >
+                                Cancel
+                            </Button>
+                        </div>
                     </div>
-                    <div className='content'>
-                        <TextField
-                            inputProps={{
-                                pattern: '[0-9]*',
-                                min: 0,
-                                max: 100,
-                                title: 'Enter valid quantity.'
-                            }}
-                            type="number"
-                            size="small"
-                            sx={{ maxWidth: 120, margin: '0 10px' }}
-                            //value={quantity}
-                            //onChange={e => setQuantity(parseInt(e.target.value))}
-                        />
-                    </div>
-                    <div className='content'>
-                        {/* <p>{quantity === 0 ? "-" : product && selectedProduct && quantity * selectedProduct.product_price}</p> */}
-                    </div>
-                    <div className='content'>
-                        <Button
-                            variant="contained"
-                            sx={{ margin: '10px' }}
-                        >
-                            Add
-                        </Button>
-                    </div>
-                </div>
-                <div className="cus-action">
-                    <Button
-                        variant="contained"
-                        onClick={handleClose}
-                    >
-                        Close
-                    </Button>
-                </div>
+                </form>
             </Box>
         </Backdrop>
     )
